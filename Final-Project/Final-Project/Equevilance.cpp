9f962ee5f255aafd475d88cd29e02f8781c5cf26
aseel seef <seef87as@gmail.com>
2019-12-30 21:38:11 +0200

    pseudo code member ship controller Equavilance controller

--- Final-Project/Final-Project/Equevilance.cpp
+++ Final-Project/Final-Project/Equevilance.cpp
@@ -1,1 +1,89 @@
-#include "Equevilance.h"
+﻿#include "Equevilance.h"
+
+/*
+
+function Equevilance(string path){
+	read file from path 
+	build Automaton LDVFA 
+	load defult automaton TDVFA
+	call product_automata(LDVFA,TDVFA)
+	if result is true 
+		return Yes 
+	else 
+		return No the counter example
+}
+
+**************************************************************
+
+function product_automata(Automaton LDVFA,Automaton TDVFA){
+	check equality of two automata by two way aggregation 
+	define flag_first_way=0
+	define flag_second_way=0
+	first way:
+	call complement_automata(LDVFA)
+	call Intersection_automata(CLDVFA,TDVFA)
+	call check_emptiness(INA)
+	if L(INA) is empty 
+		then flag_first_way=1
+	else 
+		call BFS algorithm set counter example 
+		return counter example 
+		return false 
+
+	second way :
+	call complement_automata(TDVFA)
+	call Intersection_automata(LDVFA,CTDVFA)
+	call check_emptiness(INA)
+		if L(INA) is empty
+		then flag_second_way=1
+	else
+		call BFS algorithm set counter example
+		return counter example
+		return false
+	if flag_first_way=1 and flag_second_way=1 
+		then return true
+}
+
+**************************************************************
+
+function complement_automata(Automaton A){
+	for each state that NOT accept state do:
+		convert state to accept 
+	end for
+	for each accept state do:
+		convert accept state to reject state 
+	end for
+}
+
+**************************************************************
+
+function Intersection_automata(Automaton LDVFA,Automaton TDVFA){
+	define new intersection INA automata 
+	if bounded variable size in LDVFA NOT equals to  bounded variable size in TDVFA 
+		then call expand_variablesSet(LDVFA)
+	make cross algorithm between two automata
+	for each state q in TDVFA pattern automata and state p in LDVFA pattern automata do:
+		add qipi state to INA 
+		if qi and pi is accept state 
+			then make qipi accept state 
+		make new transition in INA according to δ(qi,σ) and δ(pi,σ) 
+	end for
+	return INA		
+}
+
+
+**************************************************************
+function check_emptiness(Automaton INA){
+	define F set of accept states of INA
+	build new set R using run BFS algorthim to collect all reachable states of INA
+	if F∩R is empty 
+		then return true 
+	else 
+		return false 
+}
+
+**************************************************************
+function expand_variablesSet(Automaton LDVFA){
+
+}
+*/
--- Final-Project/Final-Project/MemberShip.cpp
+++ Final-Project/Final-Project/MemberShip.cpp
@@ -1,4 +1,56 @@
 #include "MemberShip.h"
 
+/* pseudo code
+*******************************************
+
+function memberShip(string path) {
+	read file from location path into a string w
+	call convert_concrete_to_pattern(w)
+	call run_w_on_A(w)
+	if the run stoped at accept state 
+		then return Yes
+	else 
+		return No 
+}
+
+*******************************************
+function convert_concrete_to_pattern(string w){
+	define u as new empty string
+	define k as the number of bounded variables of the default automata 
+	for each character ch in w do:
+		if ch a constant letter 
+			then concatenate to u, ch
+		else 
+			concatenate to u xi where i is a running index 
+		if ch is assigned to one of the xi's 
+			then concatenate to u, same xi 
+		else 
+			concatenate to u, xi+1
+		if i=k 
+			then check if ch is assigned to one of the xi's 
+				then concatenate to u same xi
+			else concatenate to u, y 
+	end for
+	return u
+}
+
+*******************************************
+function run_w_on_A(string w){
+	load default automata A 
+	start frong the initial state of A, q0
+	for each character ch in w do:
+		go to qi in the states linked list 
+		if there is transition with ch and qi 
+			then make transition to the next state in the transition linked list 
+		search for the next state in the states linked list 
+	end for
+	if qi is accept state 
+		then return true 
+	else 
+		return false
+}
+
+
+*/
 
 
