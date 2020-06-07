fe5a0d806f00fabf781c4c6896ff8427b70f047c
mhmod <mh_mouds@hotmail.com>
2020-06-07 19:41:40 +0300

    .

--- Final-Project/Final-Project/Controller.cpp
+++ Final-Project/Final-Project/Controller.cpp
@@ -176,7 +176,8 @@ string Controller::analyze_file(string Temp_argv_File_Location, char split_symbo
 }
 
 
-
+//first function set the default/teacher automaton file 
+//1: file location  2:split symbol 
 void Controller::initialze_System(string default_Automaton_File_Location,char split_symbol)
 {
 	default_Automaton_file.setFile(default_Automaton_File_Location);
