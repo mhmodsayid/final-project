2c90c19373be67839fb4dd96ae709d1b96fc2654
aseel seef <seef87as@gmail.com>
2020-06-12 14:34:43 +0300

    python design

--- Final-Project/Python/Python.py
+++ Final-Project/Python/Python.py
@@ -7,21 +7,31 @@ from subprocess import Popen, PIPE
 from tkinter import * 
 from tkinter.filedialog import askopenfile
 from tkinter.ttk import Style
-#from PIL import ImageTk
+import os
+import urllib.request
+import requests
+import shutil
+
+
 
 global Control_screen
 global file_path2
 root = Tk() 
 file_path2=StringVar()
-root.geometry('500x300')
+root.geometry('450x250')
 root.resizable(False,False)
 root.title("Initialization System")
 default=""
 learner=""
 
-#img = ImageTk.PhotoImage(Image.open("logo.jpg"))
-#panel = Label(root, image = img)
-#panel.pack(side = TOP, fill = "both", expand = "yes")
+#urllib.request.urlretrieve(image_url, "logo.jpg")
+#url = 'https://braude.ac.il/wp-content/uploads/2019/06/braude-logo-small-1.jpg'
+#r = requests.get(url,verify=False)
+#urllib.request.urlretrieve("https://braude.ac.il/wp-content/uploads/2019/06/braude-logo-small-1.jpg", "braude-logo-small-1.jpg")
+#widget = Label(root, compound='top')
+#widget.lenna_image_png =PhotoImage(file=local_file)
+#widget['image'] = widget.lenna_image_png
+#widget.pack()
 
 l1=Label(root, text="Learning DVFA \n \t Teacher system", font = "Cambria 16 bold italic",anchor=CENTER)
 l2=Label(root,  text="System helps the learner by answering membership queries\nand eqivalnce queries.",fg ="navy",font = "Cambria 12",justify=LEFT)
@@ -29,17 +39,17 @@ l1.pack()
 l2.place(x=0, y=90,anchor = NW)
 
 btnBrowseD = Button(root, text ='Browse default automaton',justify=CENTER,activebackground="light blue" ,command = lambda:open_file())
-btnBrowseD.place(relx = 0.25, rely = 0.55, anchor = CENTER)
-e1=Entry(root, textvariable=file_path2,relief=tk.SUNKEN, width=40)
-e1.place(relx = 0.65, rely = 0.55, anchor = CENTER)
-btnInitialize = Button(root, text ='Initialize', command = lambda:Initialze(),state=DISABLED)
-btnInitialize.place(relx = 0.5, rely = 0.7, anchor = CENTER)
+btnBrowseD.place(relx = 0.2, rely = 0.65, anchor = CENTER)
+e1=Entry(root, textvariable=file_path2,relief=tk.SUNKEN, width=45)
+e1.place(relx = 0.68, rely = 0.65, anchor = CENTER)
+btnInitialize = Button(root, text ='Initialize',foreground="green", command = lambda:Initialze(),state=DISABLED)
+btnInitialize.place(relx = 0.5, rely = 0.8, anchor = CENTER)
 
 exitButton = Button(root, text="Exit",foreground="red", background="white", command = lambda:close_window(root))
-exitButton.place(x=450, y=250)
+exitButton.place(x=410, y=220)
 
 
-
+    
 def Initialze():
     global file_path
     global BtnRun
@@ -48,19 +58,19 @@ def Initialze():
     Control_screen = Toplevel(root)
     Control_screen.resizable(False,False)
     Control_screen.title("DVFA queries")
-    Control_screen.geometry("500x300")
+    Control_screen.geometry('450x250')
     Label(Control_screen, text="Learning DVFA \n \t Teacher system",font = "Cambria 16 bold italic",anchor=CENTER).pack()
     
     btnBrowseL = Button(Control_screen, text ='Browse learner automaton',justify=CENTER,activebackground="light blue" , command = lambda:open_file2())
-    btnBrowseL.place(relx = 0.25, rely = 0.5, anchor = CENTER)
-    Learner_path = Entry(Control_screen, textvariable=file_path,width=40)
-    Learner_path.place(relx = 0.65, rely = 0.5, anchor = CENTER)
+    btnBrowseL.place(relx = 0.2, rely = 0.5, anchor = CENTER)
+    Learner_path = Entry(Control_screen, textvariable=file_path,width=45)
+    Learner_path.place(relx = 0.68, rely = 0.5, anchor = CENTER)
     
-    BtnRun = Button(Control_screen, text ='Execute Algorithm', command = lambda:RunAlg(),state=DISABLED)
+    BtnRun = Button(Control_screen, text ='Execute Algorithm',foreground="green" ,command = lambda:RunAlg(),state=DISABLED)
     BtnRun.place(relx = 0.5, rely = 0.7, anchor = CENTER) 
 
     exitButton = Button(Control_screen, text="Exit",foreground="red", background="white", command = lambda:close_window(Control_screen))
-    exitButton.place(x=450, y=250)
+    exitButton.place(x=410, y=220)
  
     
     root.withdraw();
