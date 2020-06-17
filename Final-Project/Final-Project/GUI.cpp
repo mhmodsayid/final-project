64a5da4f982b97272d829e417092f7c8ae12731c
mhmod <mh_mouds@hotmail.com>
2020-06-17 23:40:27 +0300

    fix AA

--- Final-Project/Final-Project/GUI.cpp
+++ Final-Project/Final-Project/GUI.cpp
@@ -1,4 +1,4 @@
-
+ï»¿
 #include <string>
 #include <algorithm>
 #include "FileManager.h"
@@ -13,11 +13,12 @@ int main(int argc, char* argv[]) {
 	//cout << argv[1];
 	Controller controller;
 	string default_Automaton_File_Location = argv[1];
-	string learner_File_Location = argv[2];
+	//string learner_File_Location = argv[2];
 
 	//string learner_File_Location = "LearnerFile.txt";
-	//string learner_File_Location = "MultyþþConcreteWord.txt";
-	//string default_Automaton_File_Location = "þþAutomatonFile.txt";
+	string learner_File_Location = "test5_notEqual_difAcceptState.txt";
+	//string learner_File_Location = "MultyÃ¾Ã¾ConcreteWord.txt";
+	//string default_Automaton_File_Location = "â€â€Default.txt";
 	char split_symbol = ',';
 	auto start_time = Clock::now();//start counting the time
 	string results;
